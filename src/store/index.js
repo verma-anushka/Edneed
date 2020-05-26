5906709e7a5e9072e279e355469202e1a86e5883
Anushka Verma <v.anushka786@gmail.com>
2020-05-26 15:51:34 +0530

    fix redux dev tools error

--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,50 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import reduxThunk from "redux-thunk";
-import rootReducer from "./reducers";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import reduxThunk from "redux-thunk";
+// import rootReducer from "./reducers";
 
-// const initialState = {};
-// const middleware = [reduxThunk];
+// // const initialState = {};
+// // const middleware = [reduxThunk];
 
-const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+// const composeEnhancers =
+//   typeof window === 'object' &&
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE ?
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+// // const store = createStore(
+// //   rootReducer,
+// //   initialState,
+// //   compose(
+// //     applyMiddleware(...middleware)
+// //     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// //   )
+// // );
+
 
 // const store = createStore(
 //   rootReducer,
-//   initialState,
-//   compose(
-//     applyMiddleware(...middleware)
+//   composeEnhancers(
+//     applyMiddleware(reduxThunk)
 //     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 //   )
 // );
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(
-    applyMiddleware(reduxThunk)
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+// export default store;
 
-export default store;
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+export function configureStore() {
+  const store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(reduxThunk),
+      // window.devToolsExtension ? window.devToolsExtension() : f => f
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+
+  return store;
+}
+
+
